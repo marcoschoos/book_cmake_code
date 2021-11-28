a2e2063f820080725479e8e8c339081e8e1e1796
marcoschoos <marcoschoos@gmail.com>
2021-11-28 10:01:05 +0100

    Fix issues with MSVC Compiler

--- Kapitel_8/Code_8.1/main.cpp
+++ Kapitel_8/Code_8.1/main.cpp
@@ -3,15 +3,15 @@
 int main()
 {
     int unused;
-    int position[2] = {1,2};
- 
-    auto [x,y] = position;
-    
-    #ifdef PRINT
-    	std::cout << x << " " << y << std::endl;
-    #else
-        std::cout << "PRINT not defined." << std::endl;
-    #endif
-        
+    int position[2] = { 1, 2 };
+
+    auto [x, y] = position;
+
+#ifdef PRINT
+    std::cout << x << " " << y << std::endl;
+#else
+    std::cout << "PRINT not defined." << std::endl;
+#endif
+
     return 0;
 }
