6225f76b7b0c8d8b658d667669b54c44c6970ef5
marcoschoos <marcoschoos@gmail.com>
2021-12-07 15:50:26 +0100

    Try to install glib on windows

--- scripts/install_glib_win.sh
+++ scripts/install_glib_win.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl https://download-fallback.gnome.org/sources/glib/2.70/glib-2.70.1.tar.xz -o glib-2.70.1.tar.xz
+curl --verbose https://download-fallback.gnome.org/sources/glib/2.70/glib-2.70.1.tar.xz -o glib-2.70.1.tar.xz
 echo test1
 tar xf glib-2.70.1.tar.xz 
 echo test2
