30284dc8104ae5e4032aad036b4b13070e084973
marcoschoos <marcoschoos@gmail.com>
2021-12-08 13:36:13 +0100

    Next try for glib on Windows

--- scripts/glib_win_install.ps1
+++ scripts/glib_win_install.ps1
@@ -1,6 +1,6 @@
 git clone https://gitlab.gnome.org/GNOME/glib.git
 
-echo "C:\Users\runneradmin\AppData\Roaming\Python\Python37\Scripts" | Out-File -FilePath $env:GITHUB_PATH -Encoding utf8 -Append
+set PATH=%PATH%;C:\Users\runneradmin\AppData\Roaming\Python\Python37\Scripts
 
 cd glib
 git checkout glib-2-70
