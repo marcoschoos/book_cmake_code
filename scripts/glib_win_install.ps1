34c4d50b0c5bac36adc247385cbc54c2572b7db1
marcoschoos <marcoschoos@gmail.com>
2021-12-08 13:29:13 +0100

    Next try for glib on Windows

--- scripts/glib_win_install.ps1
+++ scripts/glib_win_install.ps1
@@ -1,12 +1,11 @@
 git clone https://gitlab.gnome.org/GNOME/glib.git
 
-echo "C:\Users\runneradmin\AppData\Roaming\Python\Python37\Scripts" | Out-File -FilePath $env:GITHUB_PATH 
+echo "C:\Users\runneradmin\AppData\Roaming\Python\Python37\Scripts" | Out-File -FilePath $env:GITHUB_PATH -Encoding utf8 -Append
 
 cd glib
 git checkout glib-2-70
 choco install ninja
 pip3 install --user meson
--Encoding utf8 -Append
 meson _build
 
 ninja -C _build
