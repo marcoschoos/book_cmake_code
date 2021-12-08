b10e5239507c80af5eff6623137e3831997c6027
marcoschoos <marcoschoos@gmail.com>
2021-12-08 13:08:46 +0100

    Change branch

--- scripts/glib_win_install.ps1
+++ scripts/glib_win_install.ps1
@@ -1,6 +1,7 @@
-git clone https://gitlab.gnome.org/GNOME/glib/-/tree/glib-2-70
+git clone https://gitlab.gnome.org/GNOME/glib.git
 
 cd glib
+git checkout glib-2-70
 choco install ninja
 pip3 install --user meson
 meson _build
