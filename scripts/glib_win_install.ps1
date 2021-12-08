996b958a8ae0024c3f065343a4dd7a58a24dcf9f
marcoschoos <marcoschoos@gmail.com>
2021-12-08 13:15:33 +0100

    Next try for glib on Windows

--- scripts/glib_win_install.ps1
+++ scripts/glib_win_install.ps1
@@ -4,6 +4,7 @@ cd glib
 git checkout glib-2-70
 choco install ninja
 pip3 install --user meson
+c:\Windows\py.exe %LOCALAPPDATA%\AppData\Roaming\Python\Python37\Scripts\meson.py
 meson _build
 
 ninja -C _build
