c465ce950309e2a8b8d193945653d466650a045f
marcoschoos <marcoschoos@gmail.com>
2021-12-15 15:40:19 +0100

    Fix copy paste error when adding include guards

--- Kapitel_10/Code_10.3/include/mathe.h
+++ Kapitel_10/Code_10.3/include/mathe.h
@@ -1,16 +1,17 @@
-#ifndef MATHFUNCTIONS_H
-#define MATHFUNCTIONS_H
+#ifndef MAINBIB_H
+#define MAINBIB_H
 
-public:
-    int addieren(int a, int b);
-    int addieren_advanced(int a, int b, int c);
+class Mathe {
+    public:
+        int addieren(int a, int b);
+        int addieren_advanced(int a, int b, int c);
 
-    int subtrahieren(int a, int b);
-    int subtrahieren_advanced(int a, int b, int c);
+        int subtrahieren(int a, int b);
+        int subtrahieren_advanced(int a, int b, int c);
 
-    int addieren_subtrahieren(int a, int b, int c);
+        int addieren_subtrahieren(int a, int b, int c);
 
-    int multiplizieren(int a, int b);
+        int multiplizieren(int a, int b);
 };
 
-#endif // MATHFUNCTIONS_H
+#endif // MAINBIB_H
